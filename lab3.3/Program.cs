01004c35d74e93cb0289225a2141a2fb8880f231
user <user@DESKTOP-DV6KRKO>
2024-11-14 17:40:19 +0200

    Добавьте файлы проекта.

---
+++ lab3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    Console.WriteLine("введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите кол-во повторений: ");
+    double n = double.Parse(Console.ReadLine());
+    double s = 0;
+    double fact = 1; 
+    double step = 1; 
+    for (int i = 1; i <= n; i += 2) 
+    {
+        
+        fact *= i;
+        step *= (2 * x);
+        s += step / fact;
+
+    }
+    Console.WriteLine($"Сумма первых {n} членов ряда: {s}");
+}
+catch (Exception e)
+{
+    Console.WriteLine("ошибочка 404....");
+}
